7ee2e4c047b819467a24b7b31a7e58acebbc7173
Dorothee Schneider <ds.lite45@gmail.com>
2023-11-16 15:09:08 +0100

    [func] add TTL on youbook layout

--- Services/VarnishManager.php
+++ Services/VarnishManager.php
@@ -151,7 +151,7 @@ class VarnishManager
     {
         $return = $section_content['acf_fc_layout'];
         foreach ($section_content as $field => $value) {
-            if (strpos($field, 'field_5b27a67203e48') !== false && $value == 'random' || strpos($field, 'field_64df5e9a12a3c') !== false && $value == 'random') {
+            if (strpos($field, 'field_5b27a67203e48') !== false && $value == 'random' || strpos($field, 'field_64df5e9a12a3c') !== false && $value == 'random' || $field['acf_fc_layout'] == 'youbook_focus') {
                 $return .= '_random';
                 break;
             }
@@ -164,9 +164,9 @@ class VarnishManager
     {
         if ($acf_fc_layout == 'auto_focus_sheets' || $acf_fc_layout == 'manual_focus_minisheet') {
             return WOODY_VARNISH_CACHING_TTL_FOCUSSHEET;
-        } elseif ($acf_fc_layout == 'auto_focus' || $acf_fc_layout == 'auto_focus_topics') {
+        } elseif ($acf_fc_layout == 'auto_focus' || $acf_fc_layout == 'auto_focus_topics' || $acf_fc_layout == 'youbook_focus') {
             return WOODY_VARNISH_CACHING_TTL_FOCUS;
-        } elseif ($acf_fc_layout == 'auto_focus_random' || $acf_fc_layout == 'auto_focus_topics_random' || $acf_fc_layout == 'profile_focus_random') {
+        } elseif ($acf_fc_layout == 'auto_focus_random' || $acf_fc_layout == 'auto_focus_topics_random' || $acf_fc_layout == 'profile_focus_random' || $acf_fc_layout == 'youbook_focus_random') {
             return WOODY_VARNISH_CACHING_TTL_FOCUSRANDOM;
         } elseif ($acf_fc_layout == 'weather') {
             return WOODY_VARNISH_CACHING_TTL_WEATHERPAGE;
